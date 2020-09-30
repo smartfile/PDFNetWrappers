d943bb2588ad29ea2f2d36f0918e491a2329ff44
GitLab API <GitLab@Lumanox.com>
2020-09-30 00:35:51 +0000

    Initial Commit

---
+++ Build/PDFNetPython/CMakeFiles/PDFNetPython.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PDFNetPython.dir/PDFNetPython.cpp.o"
+  "../lib/_PDFNetPython.pdb"
+  "../lib/_PDFNetPython.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PDFNetPython.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
